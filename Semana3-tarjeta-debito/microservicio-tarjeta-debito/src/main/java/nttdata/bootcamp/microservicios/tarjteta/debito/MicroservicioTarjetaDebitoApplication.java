2be0508f85434b447c4405b61ff4a215e357c523
Daniel Augusto Prado Cussi <dpradocu@emeal.nttdata.com>
2022-04-25 11:55:37 -0500

    commit microservice-tarjeta-debito-daniel

---
+++ Semana3-tarjeta-debito/microservicio-tarjeta-debito/src/main/java/nttdata/bootcamp/microservicios/tarjteta/debito/MicroservicioTarjetaDebitoApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.tarjteta.debito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioTarjetaDebitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioTarjetaDebitoApplication.class, args);
+	}
+
+}
---
+++ Semana3-tarjeta-debito/microservicio-tarjeta-debito/src/test/java/nttdata/bootcamp/microservicios/tarjteta/debito/MicroservicioTarjetaDebitoApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.tarjteta.debito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTarjetaDebitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
